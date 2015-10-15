70869809af27191b87a60ef8907a969ca1ceaf27
Geert Eltink <xtreamwayz@gmail.com>
2015-10-15 13:49:45 +0200

    Install z-ray plugins

--- php.sh
+++ php.sh
@@ -27,14 +27,32 @@ sudo mv composer.phar /usr/bin/composer
 if [ ! -f zray-php5.6-Ubuntu-14.04-x86_64.tar.gz ]; then
     sudo wget --progress=bar:force http://downloads.zend.com/zray/1208/zray-php5.6-Ubuntu-14.04-x86_64.tar.gz
 fi
-sudo tar xvfz zray-php5.6-Ubuntu-14.04-x86_64.tar.gz -C /opt
+if [ ! -d /opt/zray ]; then
+    sudo tar xvfz zray-php5.6-Ubuntu-14.04-x86_64.tar.gz -C /opt
+    sudo chown -R www-data:www-data /opt/zray
+fi
 sudo cp -f /opt/zray/zray-ui.conf /etc/apache2/sites-available
 sudo chmod 644 /etc/apache2/sites-available/zray-ui.conf
 sudo a2ensite zray-ui.conf
-sudo chown -R www-data:www-data /opt/zray
 sudo ln -sf /opt/zray/zray.ini /etc/php5/fpm/conf.d/zray.ini
 # Disable z-ray in cli
 #sudo ln -sf /opt/zray/zray.ini /etc/php5/cli/conf.d/zray.ini
+
+# Z-Ray OPcache plugin
+if [ ! -d /opt/zray/runtime/var/plugins/OPcache ]; then
+    sudo git clone https://github.com/janatzend/Z-Ray-OPcache.git /opt/zray/runtime/var/plugins/OPcache
+else
+    cd /opt/zray/runtime/var/plugins/OPcache
+    sudo git pull
+fi
+
+# Z-Ray Doctrine 2 plugin
+if [ ! -d /opt/zray/runtime/var/plugins/Doctrine2 ]; then
+    sudo git clone https://github.com/sandrokeil/Z-Ray-Doctrine2.git /opt/zray/runtime/var/plugins/Doctrine2
+else
+    cd /opt/zray/runtime/var/plugins/Doctrine2
+    sudo git pull
+fi
 
 # Restart
 sudo service apache2 reload
